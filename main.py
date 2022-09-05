77de78cddfd8ccc293690de6acf65391f3db05c4
emiltill <jaeemf@gmail.com>
2022-09-05 23:58:46 +0600

    hw9.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print('This is the test file for Github')
-for i in range(1, 11):
-    print(i)
+# print('This is the test file for Github')
+# for i in range(1, 11):
+#     print(i)
+
+print("I'll be a \"programmer\"")
+
+
+_home2 = 5
+print(_home2)
+
+
+president = {'name':2, "surname":3}
+president3 = {'name':3, "surname":4}
+print(president.update(president3))
